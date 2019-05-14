c671e050aff44f8ceffe33511854987e089d7c36
Nikunj <nikunj@tripoto.com>
2019-05-14 13:23:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {'begin_edit': false};
+
+    }
+    removePlaceholder(ev) {
+        this.setState({'begin_edit': true});
+    }
+    contentDidMount() {
+
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="editor">
+                    {this.state.begin_edit ? "" : <span className="placeholder-title" onClick={this.removePlaceholder.bind(this)}>Write Here</span> }
+                </div>
+            </div>
+        );
+    }
 }
-
 export default App;
